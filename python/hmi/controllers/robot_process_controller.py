98dee85c6cc0e850d65bf3f5b060bc4947e3671e
María José Bogantes Murillo <mariab9@estudiantec.cr>
2026-06-26 18:34:16 -0600

    Use doubled Z speed in HMI controller

--- python/hmi/controllers/robot_process_controller.py
+++ python/hmi/controllers/robot_process_controller.py
@@ -48,7 +48,7 @@ class RobotProcessController(QObject):
 
         self.mapper = ActuatorMapper(
             z_pitch_m_per_rev=0.002,
-            z_speed_m_per_s=0.0060,
+            z_speed_m_per_s=0.0120,
             z_min_m=None,
             z_max_m=None,
         )
